8f2d452ae17ea95b4cbb8efab09373cbf5b08b6a
Juan Antonio <152283035+TheHuanPa@users.noreply.github.com>
2023-12-01 12:47:02 +0100

    Primer Commit

---
+++ src/main/java/interfaz/juegodepreguntasyrespuestas/JuegodePreguntasyRespuestas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package interfaz.juegodepreguntasyrespuestas;
+
+/**
+ *
+ * @author japar
+ */
+public class JuegodePreguntasyRespuestas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
